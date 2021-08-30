ccb53055cf4497bebbd6b21d0205a65d531c0d99
francis <francis.cazeres@gmail.com>
2021-08-30 17:00:14 +0200

    Menu principal

--- main_app.py
+++ main_app.py
@@ -20,12 +20,14 @@ class MyWindow(Tk):
         menu_file.add_command(label="Sauvegarder", command=self.do_something)
         menu_file.add_separator()
         menu_file.add_command(label="Quitter", command=self.quit)
-        menu_bar.add_cascade(label="Application", menu=menu_file)
+        menu_bar.add_cascade(label="Fichier", menu=menu_file)
 
         menu_tournament = Menu(menu_bar, tearoff=0)
         menu_tournament.add_command(label="Saisie d'un tournoi", command=self.do_tournament)
         menu_tournament.add_separator()
-        menu_tournament.add_command(label="Générer un tour", command=self.do_something)
+        menu_tournament.add_command(label="Générer round 2", command=self.do_something)
+        menu_tournament.add_command(label="Générer round 3", command=self.do_something)
+        menu_tournament.add_command(label="Générer round 4", command=self.do_something)
         menu_bar.add_cascade(label="Tournois", menu=menu_tournament)
 
         menu_players = Menu(menu_bar, tearoff=0)
@@ -35,6 +37,25 @@ class MyWindow(Tk):
         menu_score = Menu(menu_bar, tearoff=0)
         menu_score.add_command(label="Saisie des scores", command=self.do_score)
         menu_bar.add_cascade(label="Scores", menu=menu_score)
+
+        menu_rapport = Menu(menu_bar, tearoff=0)
+        menu_liste1 = Menu(menu_rapport, tearoff=0)
+        menu_liste2 = Menu(menu_rapport, tearoff=0)
+
+        menu_liste1.add_command(label="par ordre alphabétique")
+        menu_liste1.add_command(label="par classement")
+        menu_rapport.add_cascade(label="Liste des joueurs", underline=0, menu=menu_liste1)
+        menu_rapport.add_separator()
+
+        menu_liste2.add_command(label="par ordre alphabétique")
+        menu_liste2.add_command(label="par classement")
+        menu_rapport.add_cascade(label="Liste des joueurs d'un tournoi", menu=menu_liste2)
+        menu_rapport.add_separator()
+
+        menu_rapport.add_command(label="Liste des tournois", command=self.do_something)
+        menu_rapport.add_command(label="Liste de tous les tours d'un tournoi", command=self.do_something)
+        menu_rapport.add_command(label="Liste de tous les matches d'un tournoi", command=self.do_something)
+        menu_bar.add_cascade(label="Rapports", menu=menu_rapport)
 
         self.config(menu=menu_bar)
 
