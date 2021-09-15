ed79a56477c2f904dbc629326441ba2f31efb59e
francis <francis.cazeres@gmail.com>
2021-09-15 16:59:26 +0200

    Liste des joueurs d'un tournoi par classement

---
+++ view_tour1_class.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import tkinter as tk
+from tinydb import TinyDB
+from tkinter.constants import W
+from view_tour2_class import *
+
+db = TinyDB('db.json')
+tournament_table = db.table('tournament')
+
+class View_tour1_class(tk.Toplevel):
+
+    def __init__(self):
+        tk.Toplevel.__init__(self)
+        self.geometry("300x300")
+        self.title("Choisir un tournoi")
+        self.construct()
+
+    def construct(self):
+        serialized_tournament = tournament_table.all()
+
+        i = 0
+        self.var = {}
+        status = tk.StringVar()
+        for item in serialized_tournament:
+            self.var[item['name']] = status
+            self.indice = tk.Radiobutton(self, text=item['name'], 
+                        variable=status, value=item['name'], command=lambda: self.selection(status.get()))
+            self.indice.pack(anchor=W)
+            i += 1 
+
+        self.bouton1 = tk.Button(self, text="Quitter", command=self.quit)
+        self.bouton1.pack()
+        self.bouton2 = tk.Button(self, text="Valider", command=lambda: self.valid(status.get()))
+        self.bouton2.pack()
+
+    def selection(self, value):
+        pass
+
+    def valid(self, value):
+        name_tournament = value
+        view_class(name_tournament)
+            
+    def quit(self):
+        self.destroy()
+    
+
+if __name__ == "__main__":  
+
+    app = View_tour1_class()
+
+
