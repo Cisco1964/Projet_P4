b2362bc72a05b7b2e6284ea40a2f6a70fe9b245b
francis <francis.cazeres@gmail.com>
2021-09-15 16:53:34 +0200

    Menu principal

--- main_app.py
+++ main_app.py
@@ -10,7 +10,9 @@ from score import Score
 from view_players_alpha import Players_alpha
 from view_players_class import Players_class
 from view_tournament import View_tournament
-from view_tournament1_players import View_tournament1_players
+from view_tour1_alpha import View_tour1_alpha
+from view_tour1_class import View_tour1_class
+from view_tour_round import *
 
 
 class MyWindow(Tk):
@@ -28,15 +30,17 @@ class MyWindow(Tk):
         menu_file = Menu(menu_bar, tearoff=0)
         menu_file.add_command(label="Sauvegarder", command=self.do_save)
         menu_file.add_separator()
+        menu_file.add_command(label="Supprimer les tables", command=self.do_clear)
+        menu_file.add_separator()
         menu_file.add_command(label="Quitter", command=self.quit)
         menu_bar.add_cascade(label="Fichier", menu=menu_file)
 
         menu_tournament = Menu(menu_bar, tearoff=0)
         menu_tournament.add_command(label="Saisie d'un tournoi", command=self.do_tournament)
         menu_tournament.add_separator()
-        menu_tournament.add_command(label="Générer round 2", command=self.do_something)
-        menu_tournament.add_command(label="Générer round 3", command=self.do_something)
-        menu_tournament.add_command(label="Générer round 4", command=self.do_something)
+        menu_tournament.add_command(label="Générer round 2", command=self.do_round2)
+        menu_tournament.add_command(label="Générer round 3", command=self.do_round3)
+        menu_tournament.add_command(label="Générer round 4", command=self.do_round4)
         menu_bar.add_cascade(label="Tournois", menu=menu_tournament)
 
         menu_players = Menu(menu_bar, tearoff=0)
@@ -58,7 +62,7 @@ class MyWindow(Tk):
         menu_rapport.add_separator()
 
         menu_liste2.add_command(label="par ordre alphabétique", command=self.do_view_joueurs_t_alpha)
-        menu_liste2.add_command(label="par classement", command=self.do_something)
+        menu_liste2.add_command(label="par classement", command=self.do_view_joueurs_t_class)
         menu_rapport.add_cascade(label="Liste des joueurs d'un tournoi", menu=menu_liste2)
         menu_rapport.add_separator()
 
@@ -74,6 +78,15 @@ class MyWindow(Tk):
 
     def do_tournament(self):
         Tournament()
+
+    def do_round2(self):
+        view_round("round2")
+
+    def do_round3(self):
+        view_round("round3")
+
+    def do_round4(self):
+        view_round("round4")
 
     def do_players(self):
         Players()
@@ -94,13 +107,19 @@ class MyWindow(Tk):
         View_tournament()
 
     def do_view_joueurs_t_alpha(self):
-        View_tournament1_players()
+        View_tour1_alpha()
+
+    def do_view_joueurs_t_class(self):
+        View_tour1_class()
 
     def do_save(self):
         files = [('All Files', '*.*'), 
              ('Python Files', '*.py'),
              ('Text Document', '*.txt')]
         file = asksaveasfile(filetypes = files, defaultextension = files)
+
+    def do_clear(self):
+        print("Menu clicked")
   
 
     def quit(self):
