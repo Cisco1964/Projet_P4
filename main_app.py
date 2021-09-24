3a276142b1ed431ae530a1fb69dd8a2f5d8f84c2
francis <francis.cazeres@gmail.com>
2021-09-24 18:40:20 +0200

    Menu principal

--- main_app.py
+++ main_app.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 
 from tkinter import *
-from tkinter import messagebox
 from tkinter.filedialog import asksaveasfile
 from tournament import Tournament
 from players import Players
 from update_players import Update_players
 from score import update_score
-from view_all_players import *
+from view_all_players import view_all
 from view_tournament import View_tournament
-from view_choice_tour import *
-from view_choice_round import *
-from view_round import *
-from view_clear_table import *
-
+from view_choice_tour import view_choice
+from view_choice_round import view_gen_round
+from view_round import view_round
+from view_clear_table import clear_table
 
 
 class MyWindow(Tk):
 
     def __init__(self):
+
         Tk.__init__(self)
         self.create_menu_bar()
 
@@ -26,6 +25,7 @@ class MyWindow(Tk):
         self.title("Projet P4")
 
     def create_menu_bar(self):
+
         menu_bar = Menu(self)
 
         menu_file = Menu(menu_bar, tearoff=0)
@@ -40,7 +40,7 @@ class MyWindow(Tk):
         menu_tournament.add_command(label="Saisie d'un tournoi", command=self.do_tournament)
         menu_tournament.add_separator()
         menu_tournament.add_command(label="Générer tour suivant", command=self.do_other_round)
-        menu_tournament.add_command(label="Visualiser le tour en cours", command=self.do_round1)
+        menu_tournament.add_command(label="Visualiser le tour en cours", command=self.do_round)
         menu_bar.add_cascade(label="Tournois", menu=menu_tournament)
 
         menu_players = Menu(menu_bar, tearoff=0)
@@ -67,59 +67,93 @@ class MyWindow(Tk):
         menu_rapport.add_separator()
 
         menu_rapport.add_command(label="Liste des tournois", command=self.do_view_tournament)
-        menu_rapport.add_command(label="Liste de tous les tours d'un tournoi", command=self.do_something)
-        menu_rapport.add_command(label="Liste de tous les matches d'un tournoi", command=self.do_something)
+        menu_rapport.add_command(label="Liste de tous les tours d'un tournoi", command=self.do_view_round_tour)
+        menu_rapport.add_command(label="Liste de tous les matches d'un tournoi", command=self.do_view_match_tour)
         menu_bar.add_cascade(label="Rapports", menu=menu_rapport)
 
         self.config(menu=menu_bar)
 
-    def do_something(self):
-        print("Menu clicked")
-
     def do_tournament(self):
+
+        ''' Tournois'''
         Tournament()
 
-    def do_round1(self):
+    def do_round(self):
+
+        ''' Visualiser un round '''
         view_round()
 
     def do_other_round(self):
+
+        ''' Générer le tour suivant '''
         view_gen_round()
 
     def do_players(self):
+
+        ''' Saisie des joueurs '''
         Players()
 
     def do_maj_players(self):
+
+        ''' Mise à jour du classement des joueurs '''
         Update_players()
 
     def do_score(self):
+
+        ''' Saisie des scores '''
         update_score()
 
     def do_joueurs_alpha(self):
+
+        ''' Liste des joueurs par nom '''
         view_all("nom")
 
     def do_joueurs_class(self):
+
+        ''' Liste des joueurs par classement '''
         view_all("classement")
 
     def do_view_tournament(self):
+
+        ''' Liste des tournois '''
         View_tournament()
 
     def do_view_joueurs_t_alpha(self):
+
+        ''' Liste des joueurs d'un tournoi par nom '''
         view_choice("nom")
 
     def do_view_joueurs_t_class(self):
+
+        ''' Liste des joueurs d'un tournoi par classement '''
         view_choice("classement")
 
+    def do_view_round_tour(self):
+
+        ''' Liste de tous les rounds d'un tournoi '''
+        view_choice("round")
+
+    def do_view_match_tour(self):
+
+        ''' Liste de tous les matchs d'un tournoi '''
+        view_choice("match")
+
     def do_save(self):
+
+        ''' Sauvegarde des fichiers'''
         files = [('All Files', '*.*'), 
              ('Python Files', '*.py'),
              ('Text Document', '*.txt')]
         file = asksaveasfile(filetypes = files, defaultextension = files)
 
     def do_clear(self):
+
+        ''' Clear table'''
         clear_table()
   
-
     def quit(self):
+
+        ''' Exit '''
         self.destroy()
 
 
