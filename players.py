fbff85fbebc2b907d51801455bbedb9704d2f5d0
francis <francis.cazeres@gmail.com>
2021-09-24 18:38:58 +0200

    Saisie des joueurs

--- players.py
+++ players.py
@@ -14,18 +14,20 @@ players_table = db.table('players')
 class Players(tk.Toplevel):
 
     def __init__(self):
+
         tk.Toplevel.__init__(self)
         self.title("Joueurs")
         self.creer_widgets()
 
     def valid(self):
+
         if (self.nom.get() == "" 
-        or self.prenom.get() == "" 
-        or self.datenaissance.get() == "" 
-        or self.sex.get() == "" 
-        or self.classement.get() == 0
-        or self.classement.get() == ""):
-            showerror("Résultat", "Saisir tous les champs.\nVeuillez recommencer !")
+            or self.prenom.get() == "" 
+            or self.datenaissance.get() == "" 
+            or self.sex.get() == "" 
+            or self.classement.get() == 0
+            or self.classement.get() == ""):
+                showerror("Résultat", "Saisir tous les champs.\nVeuillez recommencer !")
         else:
             bool = self.numberonly()
             if bool:
@@ -41,6 +43,7 @@ class Players(tk.Toplevel):
                     showwarning("Joueurs", "Vous avez déjà saisi 8 joueurs !")
                 
     def insert_user(self, compteur):
+
         serialized_players = {
             'indice': compteur,
             'nom': self.nom.get(), 
@@ -49,19 +52,19 @@ class Players(tk.Toplevel):
             'sexe': self.sex.get(),
             'classement': int(self.classement.get()),
         }
-        print(serialized_players)
         players_table.insert(serialized_players)
         self.reset()
         
     def reset(self):
+
         self.nom.set("")
         self.prenom.set("")
         self.datenaissance.set_date(datetime.date.today().strftime("%d/%m/%Y"))
         self.sex.set("")
         self.classement.set(0)
-        print("reset")
 
     def numberonly(self):
+
         if self.classement.get() in "123456789":    
             return True
         else:
@@ -69,6 +72,7 @@ class Players(tk.Toplevel):
             return False
             
     def creer_widgets(self):
+
         self.label1 = tk.Label(self, text="Nom de famille").grid(row=0)
         self.label2 = tk.Label(self, text="Prénom").grid(row=1)
         self.label3 = tk.Label(self, text="Date de naissance").grid(row=2)
@@ -93,7 +97,7 @@ class Players(tk.Toplevel):
         self.champ2.grid(row=1, column=1, padx=5, pady=5)
         
         self.datenaissance = DateEntry(self, width=12, background='darkblue',
-                    foreground='white', borderwidth=2)
+                                       foreground='white', borderwidth=2)
         self.datenaissance.grid(row=2, column=1, padx=5, pady=5)
 
         self.champf = tk.Radiobutton(self, text="F", variable=self.sex, value="F")
@@ -105,6 +109,8 @@ class Players(tk.Toplevel):
         self.champ4.grid(row=5, column=1, padx=5, pady=5)    
 
     def quit(self):
+
+        ''' Exit'''
         self.destroy()
 
 
@@ -112,6 +118,5 @@ if __name__ == "__main__":
     app = Players()
     app = tk.Toplevel()
     app.lift()
-    app.attributes('-topmost',True)
-    app.after_idle(app.attributes,'-topmost',False)
-
+    app.attributes('-topmost', True)
+    app.after_idle(app.attributes, '-topmost', False)
