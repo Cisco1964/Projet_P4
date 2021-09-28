c14ca58154e9732743808b6cfefe1e0f86c65e2d
francis <francis.cazeres@gmail.com>
2021-09-28 15:46:19 +0200

    Visualiser le tour en cours

--- view_round.py
+++ view_round.py
@@ -4,29 +4,29 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter.messagebox import showerror
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 
 db = TinyDB('db.json')
 
 
 class View_round(tk.Toplevel):
 
-    def __init__(self, tournament, round, serialized_round):
+    def __init__(self, id_tournament, round, serialized_round):
+
         tk.Toplevel.__init__(self)
-        self.title("{} du tournoi de {}".format(round, tournament))
+        # research the name of the tournament
+        name_tournament = search_tournament(id_tournament)
+        self.title("{} du tournoi de {}".format(round, name_tournament))
         style = ttk.Style()
         style.theme_use("default")
         style.map("Treeview")
-        self.construct(tournament, serialized_round)
+        self.construct(serialized_round)
 
-    def construct(self, tournament, serialized_round):
+    def construct(self, serialized_round):
+
         """Construction du treeview"""
-        tv = ttk.Treeview(
-        self, 
-        columns=(1, 2, 3, 4, 5), 
-        show='headings', 
-        height=4
-        )
+        tv = ttk.Treeview(self, columns=(1, 2, 3, 4, 5),
+                          show='headings', height=4)
         tv.pack()
 
         tv.heading(1, text='nom')
@@ -38,10 +38,10 @@ class View_round(tk.Toplevel):
         tournament_players = db.table('players')
         serialized_players = tournament_players.all()
 
-        joueurs = (serialized_round[0]['joueurs'])
+        players = (serialized_round[0]['joueurs'])
         match = []
         table_match = []
-        for item in joueurs:
+        for item in players:
             for i in item:
                 match.append(search(i, serialized_players))
             table_match.append(match)
@@ -50,25 +50,35 @@ class View_round(tk.Toplevel):
         i = 0
         for item in table_match:
             tv.insert(parent='', index=i, iid=i, values=(item[0]['nom'], item[0]['prenom'], "VS",
-                item[1]['nom'], item[1]['prenom'])) 
+                      item[1]['nom'], item[1]['prenom']))
             i += 1
 
+
 def search(i, serialized_players):
-    """Récupération des éléments du joueur"""
+
+    """Collecting player items"""
     for element in serialized_players:
         if element['indice'] == i:
             return element
 
+
+def search_tournament(id_tournament):
+
+    '''Research the name of the tournament'''
+    tournament_table = db.table('tournament')
+    serialized_tournament = tournament_table.all()
+    for element in serialized_tournament:
+        if element['id'] == id_tournament:
+            return element['name']
+
+
 def view_round():
+
     round_table = db.table('round_match')
     serialized_round = round_table.all()
-    try:
-        serialized_round == []
-    except IndexError:
+    if serialized_round == []:
         showerror("Résultat", "Aucun tour en attente !")
     else:
-        tournament = serialized_round[0]['tournament']
+        id_tournament = serialized_round[0]['id']
         round = serialized_round[0]['round']
-        View_round(tournament, round, serialized_round)
-
-        
+        View_round(id_tournament, round, serialized_round)
