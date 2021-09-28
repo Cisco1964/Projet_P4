84443b98167a93c41ffea233a378abd4cb727af5
francis <francis.cazeres@gmail.com>
2021-09-28 15:25:51 +0200

    Saisie des joueurs

--- players.py
+++ players.py
@@ -21,32 +21,32 @@ class Players(tk.Toplevel):
 
     def valid(self):
 
-        if (self.nom.get() == "" 
-            or self.prenom.get() == "" 
-            or self.datenaissance.get() == "" 
-            or self.sex.get() == "" 
+        if (self.nom.get() == ""
+            or self.prenom.get() == ""
+            or self.datenaissance.get() == ""
+            or self.sex.get() == ""
             or self.classement.get() == 0
             or self.classement.get() == ""):
-                showerror("Résultat", "Saisir tous les champs.\nVeuillez recommencer !")
+            showerror("Résultat", "Saisir tous les champs.\nVeuillez recommencer !")
         else:
             bool = self.numberonly()
             if bool:
                 elements = len(players_table)
                 if elements < 8:
                     if elements == 0:
-                        compteur = 1 
+                        compteur = 1
                     else:
                         compteur = elements + 1
                     print(compteur)
                     self.insert_user(compteur)
                 else:
                     showwarning("Joueurs", "Vous avez déjà saisi 8 joueurs !")
-                
+
     def insert_user(self, compteur):
 
         serialized_players = {
             'indice': compteur,
-            'nom': self.nom.get(), 
+            'nom': self.nom.get(),
             'prenom': self.prenom.get(),
             'datenaissance': self.datenaissance.get(),
             'sexe': self.sex.get(),
@@ -54,7 +54,7 @@ class Players(tk.Toplevel):
         }
         players_table.insert(serialized_players)
         self.reset()
-        
+
     def reset(self):
 
         self.nom.set("")
@@ -65,12 +65,12 @@ class Players(tk.Toplevel):
 
     def numberonly(self):
 
-        if self.classement.get() in "123456789":    
+        if self.classement.get() in "123456789":
             return True
         else:
             showerror("Classement invalide", "Veuillez recommencer !")
             return False
-            
+
     def creer_widgets(self):
 
         self.label1 = tk.Label(self, text="Nom de famille").grid(row=0)
@@ -78,7 +78,7 @@ class Players(tk.Toplevel):
         self.label3 = tk.Label(self, text="Date de naissance").grid(row=2)
         self.label4 = tk.Label(self, text="Sexe").grid(row=3)
         self.label5 = tk.Label(self, text="Classement").grid(row=5)
-                
+
         self.bouton = tk.Button(self, text="Quitter", command=self.quit)
         self.bouton.grid(row=10, padx=10, pady=10)
         self.bouton = tk.Button(self, text="Valider", command=self.valid)
@@ -95,7 +95,7 @@ class Players(tk.Toplevel):
         self.champ1.grid(row=0, column=1, padx=5, pady=5)
         self.champ2 = tk.Entry(self, textvariable=self.prenom)
         self.champ2.grid(row=1, column=1, padx=5, pady=5)
-        
+
         self.datenaissance = DateEntry(self, width=12, background='darkblue',
                                        foreground='white', borderwidth=2)
         self.datenaissance.grid(row=2, column=1, padx=5, pady=5)
@@ -106,7 +106,7 @@ class Players(tk.Toplevel):
         self.champm.grid(row=4, column=1)
 
         self.champ4 = tk.Entry(self, textvariable=self.classement)
-        self.champ4.grid(row=5, column=1, padx=5, pady=5)    
+        self.champ4.grid(row=5, column=1, padx=5, pady=5)
 
     def quit(self):
 
