86ece404374dcc1b011de8b4c5cf716b00d15cd6
francis <francis.cazeres@gmail.com>
2021-09-28 15:25:13 +0200

    Menu principal

--- main_app.py
+++ main_app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from tkinter import *
+from tkinter import Menu, Tk
 from tkinter.filedialog import asksaveasfile
 from tournament import Tournament
 from players import Players
@@ -36,17 +36,17 @@ class MyWindow(Tk):
         menu_file.add_command(label="Quitter", command=self.quit)
         menu_bar.add_cascade(label="Fichier", menu=menu_file)
 
+        menu_players = Menu(menu_bar, tearoff=0)
+        menu_players.add_command(label="Saisie des joueurs", command=self.do_players)
+        menu_players.add_command(label="Mise à jour classement joueurs", command=self.do_update_players)
+        menu_bar.add_cascade(label="Joueurs", menu=menu_players)
+
         menu_tournament = Menu(menu_bar, tearoff=0)
         menu_tournament.add_command(label="Saisie d'un tournoi", command=self.do_tournament)
         menu_tournament.add_separator()
         menu_tournament.add_command(label="Générer tour suivant", command=self.do_other_round)
         menu_tournament.add_command(label="Visualiser le tour en cours", command=self.do_round)
         menu_bar.add_cascade(label="Tournois", menu=menu_tournament)
-
-        menu_players = Menu(menu_bar, tearoff=0)
-        menu_players.add_command(label="Saisie des joueurs", command=self.do_players)
-        menu_players.add_command(label="Mise à jour classement joueurs", command=self.do_maj_players)
-        menu_bar.add_cascade(label="Joueurs", menu=menu_players)
 
         menu_score = Menu(menu_bar, tearoff=0)
         menu_score.add_command(label="Saisie des scores", command=self.do_score)
@@ -56,13 +56,13 @@ class MyWindow(Tk):
         menu_liste1 = Menu(menu_rapport, tearoff=0)
         menu_liste2 = Menu(menu_rapport, tearoff=0)
 
-        menu_liste1.add_command(label="par ordre alphabétique", command=self.do_joueurs_alpha)
-        menu_liste1.add_command(label="par classement", command=self.do_joueurs_class)
+        menu_liste1.add_command(label="par ordre alphabétique", command=self.do_players_name)
+        menu_liste1.add_command(label="par classement", command=self.do_players_ranking)
         menu_rapport.add_cascade(label="Liste des joueurs", underline=0, menu=menu_liste1)
         menu_rapport.add_separator()
 
-        menu_liste2.add_command(label="par ordre alphabétique", command=self.do_view_joueurs_t_alpha)
-        menu_liste2.add_command(label="par classement", command=self.do_view_joueurs_t_class)
+        menu_liste2.add_command(label="par ordre alphabétique", command=self.do_view_players_t_name)
+        menu_liste2.add_command(label="par classement", command=self.do_view_players_t_ranking)
         menu_rapport.add_cascade(label="Liste des joueurs d'un tournoi", menu=menu_liste2)
         menu_rapport.add_separator()
 
@@ -75,82 +75,80 @@ class MyWindow(Tk):
 
     def do_tournament(self):
 
-        ''' Tournois'''
+        ''' Tournament'''
         Tournament()
 
     def do_round(self):
 
-        ''' Visualiser un round '''
+        ''' View a round '''
         view_round()
 
     def do_other_round(self):
 
-        ''' Générer le tour suivant '''
+        ''' Generate the next round '''
         view_gen_round()
 
     def do_players(self):
 
-        ''' Saisie des joueurs '''
+        ''' Entering players '''
         Players()
 
-    def do_maj_players(self):
+    def do_update_players(self):
 
-        ''' Mise à jour du classement des joueurs '''
+        ''' Update player rankings '''
         Update_players()
 
     def do_score(self):
 
-        ''' Saisie des scores '''
+        ''' Entering scores '''
         update_score()
 
-    def do_joueurs_alpha(self):
+    def do_players_name(self):
 
-        ''' Liste des joueurs par nom '''
+        ''' List of players by name '''
         view_all("nom")
 
-    def do_joueurs_class(self):
+    def do_players_ranking(self):
 
-        ''' Liste des joueurs par classement '''
+        ''' List of players by ranking '''
         view_all("classement")
 
     def do_view_tournament(self):
 
-        ''' Liste des tournois '''
+        ''' List of tournaments '''
         View_tournament()
 
-    def do_view_joueurs_t_alpha(self):
+    def do_view_players_t_name(self):
 
-        ''' Liste des joueurs d'un tournoi par nom '''
+        ''' List of tournament players by name '''
         view_choice("nom")
 
-    def do_view_joueurs_t_class(self):
+    def do_view_players_t_ranking(self):
 
-        ''' Liste des joueurs d'un tournoi par classement '''
+        ''' List of players in a tournament by ranking '''
         view_choice("classement")
 
     def do_view_round_tour(self):
 
-        ''' Liste de tous les rounds d'un tournoi '''
+        ''' List of all rounds in a tournament'''
         view_choice("round")
 
     def do_view_match_tour(self):
 
-        ''' Liste de tous les matchs d'un tournoi '''
+        ''' List of all matches in a tournament '''
         view_choice("match")
 
     def do_save(self):
 
-        ''' Sauvegarde des fichiers'''
-        files = [('All Files', '*.*'), 
-             ('Python Files', '*.py'),
-             ('Text Document', '*.txt')]
-        file = asksaveasfile(filetypes = files, defaultextension = files)
+        ''' Save files'''
+        files = [('All Files', '*.*'), ('Python Files', '*.py'), ('Text Document', '*.txt')]
+        file = asksaveasfile(filetypes=files, defaultextension=files)
 
     def do_clear(self):
 
         ''' Clear table'''
         clear_table()
-  
+
     def quit(self):
 
         ''' Exit '''
