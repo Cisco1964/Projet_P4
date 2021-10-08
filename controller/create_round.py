fb572fcdd325830fd8c0cf47684c7e7118465b02
Cisco1964 <67370281+Cisco1964@users.noreply.github.com>
2021-10-08 16:52:07 +0200

    controller

---
+++ controller/create_round.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tinydb import TinyDB, Query
+from tinydb.operations import add
+import datetime
+
+db = TinyDB('db/db.json')
+
+
+def add_round(id_tournament, round, my_players):
+    print(id_tournament, round, my_players)
+    """ create round """
+    datedeb = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+    serialized_match = {'id': int(id_tournament),
+                        'round': round,
+                        'datedebut': datedeb,
+                        'joueurs': my_players}
+    db.table('round_match').insert(serialized_match)
+
+    ''''update value round'''
+    tournament_table = db.table('tournament')
+    tournament_db = Query()
+    if round != "round1":
+        tournament_table.update(add('round', round.split()), tournament_db.id == int(id_tournament))
