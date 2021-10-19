b9d2cc9f20138a9ce59847e93e35e5922df4cb1d
Cisco1964 <67370281+Cisco1964@users.noreply.github.com>
2021-10-19 16:15:23 +0200

    model

---
+++ model/create_players.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tinydb import TinyDB
+
+
+class Create_Players():
+
+    def __init__(self):
+
+        global players_table
+        db = TinyDB('db/db.json')
+        players_table = db.table('players')
+
+    def insert_user(self, compteur, arg1, arg2, arg3, arg4, arg5):
+        serialized_players = {
+            'indice': compteur,
+            'nom': arg1,
+            'prenom': arg2,
+            'datenaissance': arg3,
+            'sexe': arg4,
+            'classement': int(arg5),
+        }
+        players_table.insert(serialized_players)
+
+        return
